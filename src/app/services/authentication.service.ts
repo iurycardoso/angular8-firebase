885402eab2854c3a1f52fe21bb06266b12f7f6ed
Iury Cardoso <iurycardoso6@gmail.com>
2020-02-09 18:01:38 -0300

    Servico de login|logout|passwordrecovery adicionado

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { AngularFireAuth } from '@angular/fire/auth'
+import * as firebase from 'firebase/app'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private user: Observable<firebase.User>
+
+  constructor(
+    private afAuth: AngularFireAuth
+  ) { 
+    this.user = afAuth.authState
+  }
+
+  /** 
+   * O	método	 	signInWithEmailAndPassword()		 recebe	 os	 dois
+   * argumentos	e	retorna	uma	Promise	com	as	credenciais	do	usuário
+   * @param email 
+   * @param password 
+   */
+  login(email: string, password: string): Promise<firebase.auth.UserCredential> {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+  }
+
+  /*
+   * no	método		logout()		não	passamos	nenhum	argumento	e
+   * invocamos	 a	 função	 	signOut()		 do	 	AngularFireAuth		 para
+   * desconectar.
+   */
+  logout(): Promise<void> {
+    return this.afAuth.auth.signOut()
+  }
+
+  /**
+   * Passamos	 um	 email	 no	 parâmetro	 para	 a	 função
+	 * sendPasswordResetEmail()	.	Assim	 o	 Firebase	 envia	 um	 email
+   * com	um	link	para	redefinição	de	senhas	para	o	email	informado.
+   * @param email 
+   */
+  resetPassword(email: string) {
+    this.afAuth.auth.sendPasswordResetEmail(email)
+  }
+}
